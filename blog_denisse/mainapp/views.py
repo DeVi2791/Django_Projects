a0a3c823c0d8d03fa06d73320e55b8aba613a5d9
Denisse Villamar <de2791vi@gmail.com>
2017-02-22 11:37:41 -0500

    Creando index en nuestro proyecto usando views y urls

--- blog_denisse/mainapp/views.py
+++ blog_denisse/mainapp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+	nombre="Denisse Villamar"
+	return HttpResponse('<h1>Hola Mundo, los saluda '+nombre+'</h1>')
+
