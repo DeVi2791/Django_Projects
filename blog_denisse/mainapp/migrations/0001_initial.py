cc9ae4647a97821558c82decc5d9ca4a108e12e9
Denisse Villamar <de2791vi@gmail.com>
2017-02-24 12:15:11 -0500

    Migraciones, Modelos, Formularios

---
+++ blog_denisse/mainapp/migrations/0001_initial.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Entry',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('title', models.CharField(max_length=50)),
+                ('body', models.TextField()),
+                ('img_url', models.TextField()),
+            ],
+        ),
+    ]
--- blog_denisse/mainapp/models.py
+++ blog_denisse/mainapp/models.py
@@ -1,3 +1,11 @@
+from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Entry (models.Model):
+	title = models.CharField(max_length=50)
+	body = models.TextField()
+	img_url = models.TextField()
+
+	def __str__(self):
+		return "{},{},{},{}".format(self.id,self.title,self.body,self.img_url)
